8e11ae37f76a604fe26839fcdae857b3a64f0f53
Nikitha5399 <94245015+Nikitha5399@users.noreply.github.com>
2021-11-23 17:21:24 +0530

    Update Project_main.c

--- 3_Implementation/Project_main.c
+++ 3_Implementation/Project_main.c
@@ -1,69 +1,92 @@
-include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
- 
-int process(char you, char computer)
-{
 
-    if (you == computer)
-        return -1;
-        
-    if (you == 's' && computer == 'p')
-        return 0;
-        
-            else if (you == 'p' && computer == 's') return 1;
 
-    if (you == 's' && computer == 'z')
-        return 1;
- 
-    else if (you == 'z' && computer == 's')
-        return 0;
-
-    if (you == 'p' && computer == 'z')
-        return 0;
- 
-    else if (you == 'z' && computer == 'p')
-        return 1;
-}
+int generateRandomNumber(int n);
+int greater(char c1, char c2);
 int main()
 {
-    
-    int n;
- 
-    char you, computer, result;
- 
-    srand(time(NULL));
+    int playerScore = 0, compScore = 0, temp;
+    char playerChar, compChar;
+    char dict[] = {'s', 'p', 'z'};
+    printf("\tWelcome to the Stone Paper Scissors\n");
+    printf("\t----------------------------------\n\n");
 
-    n = rand() % 100;
+    for (int i = 0; i < 3; i++)
+    {
+        // Take player input
+        printf("Press 1 for Stone, Press 2 for Paper, Press 3 for Scissors\n\n");
+        printf("\tPlayer's turn: ");
+        scanf("%d", &temp);
+        getchar();
+        playerChar = dict[temp - 1];
+        printf(" -----------------\n");
+        printf("| You choose: %c   |\n", playerChar);
+        printf(" -----------------\n\n");
 
-    if (n < 33)
- 
-         computer = 's';
- 
-    else if (n > 33 && n < 66)
- 
-        computer = 'p';
+        //computer generate
+        printf("Press 1 for Stone, Press 2 for Paper, Press 3 for Scissors\n\n");
+        printf("\tComputer's turn\n");
+        temp = generateRandomNumber(3) + 1;
+        compChar = dict[temp - 1];
+        printf(" --------------------\n");
+        printf("| Computer choose: %c |\n", compChar);
+        printf(" --------------------\n\n");
 
+        // compater character and increment the score
+        if (greater(compChar, playerChar) == 1)
+        {
+            compScore++;
+            printf("\t\tComputer Got It!\n\n");
+        }
+        else if (greater(compChar, playerChar) == -1)
+        {
+            compScore++;
+            playerScore++;
+            printf("\t\tIt's a draw. Both got 1 point!\n\n");
+        }
+        else
+        {
+            playerScore++;
+            printf("\t\tYou Got It!\n\n");
+        }
+
+        printf(" -------------\n");
+        printf("| You: %d      |\n", playerScore);
+        printf("| Computer: %d |\n", compScore);
+        printf(" -------------\n\n");
+
+        printf("===========================================================\n\n");
+    }
+
+    printf(" -----------------\n");
+    printf("|   Final Score   |\n");
+    printf(" -----------------\n");
+    printf("|  You | Computer |\n");
+    printf("|------|----------|\n");
+    printf("|   %d  |    %d     |\n", playerScore, compScore);
+    printf(" -----------------\n\n");
+
+    // compare score
+    if (playerScore > compScore)
+    {
+        printf("\n\t -------------------\n");
+        printf("\t| You have Won the match! |\n");
+        printf("\t -------------------\n");
+    }
+    else if (playerScore < compScore)
+    {
+        printf("\n\t ------------------------\n");
+        printf("\t| You have lost the match! |\n");
+        printf("\t ------------------------\n");
+    }
     else
-        computer = 'z';
- 
-    printf("\n\n\n\n\t\t\t\tEnter s for STONE, p for PAPER and z for SCISSOR\n\t\t\t\t\t\t\t");
- 
-    scanf("%c", &you);
- 
-    result = process(you, computer);
- 
-    if (result == -1) {
-        printf("\n\n\t\t\t\tgame Draw!\n");
+    {
+        printf("\n\t -------------\n");
+        printf("\t| game Draw! |\n");
+        printf("\t -------------\n");
     }
-    else if (result == 1) {
-        printf("\n\n\t\t\t\tWow! You have won the game!\n"); 
-    }
-    else {
-        printf("\n\n\t\t\t\tOh! You have lost the game!\n");
-    }
-        printf("\t\t\t\tYOu choose : %c and Computer choose : %c\n",you, computer);
- 
+
     return 0;
 }
